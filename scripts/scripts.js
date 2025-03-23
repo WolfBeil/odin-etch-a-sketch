e634cebfed6710383c531a4384ae20004f35b6f5
WolfBeil <84537202+WolfBeil@users.noreply.github.com>
2025-03-22 21:36:54 -0400

    Make function to create grid
    A function named createGrid was declared. This function will be passed
    an argument called 'squares' that will define how many rows and columns
    the grid is going to have.
    This was made possible using a for loop for appehending the rows
    inside the grid container, and placing another for loop inside that one
    for appehending the columns inside the rows.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,3 +4,20 @@ const blocks = document.querySelectorAll(".second-cell").forEach(blocks => block
 function fill(block) {
     block.style.backgroundColor = "black";
 }
+
+const grid = document.querySelector(".grid");
+
+function createGrid(squares) {
+    for (let i = 0; i < squares; i++) {
+        const row = document.createElement("div");
+        row.classList = "row";
+        grid.appendChild(row);
+        for (let j = 0; j < squares; j++) {
+            const col = document.createElement("div");
+            col.classList = "col";
+            row.appendChild(col);
+        }
+    }
+}
+
+createGrid(5);
